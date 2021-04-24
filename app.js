930d4316c997746246e94df0d5eb053f96dfd92f
gfunkmaster <nurkicelmin@hotmail.com>
2021-04-24 18:00:48 +0200

    Finish

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 
 const diceTriggerEl = document.querySelector('.dice__trigger');
-const diceImageEl = document.querySelector('.dice_image');
+const diceImageEl = document.querySelector('.dice__image');
+
+console.log(diceImageEl);
+
+
+//Formula for random number beetween 1 to 6, plus 1 becuase javascript is zero based
+//Math.floor(Math.random() * 6) + 1; 
+/* 
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const rollDice = () => getRandomNumber(1, 6);
+
+let diceRoll = Math.floor(Math.random() * 6) + 1;
+
+console.log(diceRoll);
+ */
+
+//create a random number genaratior 
+const getRandomNumber = () => {
+    return  Math.floor(Math.random() * 6) + 1;
+}
+
+//function for trigger the button 
+const showDice = () =>{
+    diceTriggerEl.addEventListener('click', () => {
+
+        let myRandomNumber = getRandomNumber();
+        
+        diceImageEl.src = `images/dice${myRandomNumber}.jfif`
+    })
+}
+
+
+
+
+
+showDice()
